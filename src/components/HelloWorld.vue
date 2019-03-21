241db266256ac6f6728175dbf1715a9372a60d88
sunjian <15927594048m0@sina.cn>
2019-03-21 21:40:41 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>
+      Ask a yes/no question:
+      <input v-model="question">
+    </p>
+    <p>{{ answer }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      question: '',
+      answer:  'I cannot give you an answer until you ask a question!'
+    }
+  },
+  methods: {
+    see: function () {
+      this.seen = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/todo-item.vue
@@ -0,0 +1,14 @@
+<template>
+    <li>这是个代办事项</li>
+</template>
+
+<script>
+    export default {
+        name: "todo-item",
+      props: ['todo']
+    }
+</script>
+
+<style scoped>
+
+</style>
