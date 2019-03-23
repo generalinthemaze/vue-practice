ffd216b837bc31e334d3b14f4fdfe93ca6fd639f
sunjian <1226816573@qq.com>
2019-03-23 18:36:48 +0800

    use npm i to change the package.json add a iview.html to pra

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@ export default {
   data () {
     return {
       question: '',
-      answer:  'I cannot give you an answer until you ask a question!'
+      answer: 'I cannot give you an answer until you ask a question!'
     }
   },
   methods: {
